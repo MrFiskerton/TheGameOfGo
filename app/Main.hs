5f3f5cbcef01ab411b93d0298a16d00c63f45916
Roman Fiskov <Mr.Fiskerton@yandex.ru>
2019-04-10 00:56:28 +0300

    Added window helloworld

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Graphics.Gloss
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = display window background drawing
+      where
+      window = InWindow "Nice Window" (400, 400) (300, 300) -- size, position
+      background = white
+      drawing = Translate (-170) (-20) -- shift the text to the middle of the window
+                $ Scale 0.5 0.5        -- display it half the original size
+                $ Text "Hello World"   -- text to display
