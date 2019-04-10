af92f07e9ae63aaf99930a7014ebfe6a3ae66c8b
Roman Fiskov <Mr.Fiskerton@yandex.ru>
2019-04-10 07:14:50 +0300

    Added gameover screen

--- src/Runner.hs
+++ src/Runner.hs
@@ -19,9 +19,17 @@ data Player = Player { name  :: !String
 
 -- |
 goplayers :: [Player]
-goplayers  = [ Player "Black" 0 0
-             , Player "White" (6 + 1 % 2) 1
+goplayers  = [ Player "Pig" 0 0
+             , Player "Chicken" (6 + 1 % 2) 1
              ]
+
+-- |
+data GameState p = GameState { game       :: !(G.GoGameSession p)
+                             , isGameOver :: !Bool
+                             } deriving (Eq, Show)
+initialState :: GameState Player
+initialState = GameState { game = session, isGameOver = False} where
+    session = G.gosession 19 19 goplayers (Map.fromList $ zip goplayers (map komi goplayers))
 
 -- |
 both :: (a -> b) -> (a, a) -> (b, b)
@@ -34,25 +42,41 @@ start images =
     --      -> (world -> Picture)                   -- renderer
     --      -> (Event -> world -> world)            -- updater
     --      -> (Float -> world -> world) -> IO ()   -- handler
-    play window background fps game (renderer images) handler updater
+    play window background fps initialState (renderer images) handler updater
       where window = InWindow "The Game of Go" size position
             size = both (* round cellsize) fieldsize
             fieldsize = (21, 21)
             cellsize = 32 :: Float
             position = (300, 300)
-            k = 6 + 1 % 2
-            game = G.gosession 19 19 goplayers (Map.fromList $ zip goplayers (map komi goplayers))
             background = white
             fps = 30
 -- |
 viewPort = ViewPort (both (negate . (/ 2) . subtract 32) $ cellToScreen (18, 18)) 0 1 -- rotation , scale
 
+score :: G.GoGameSession Player -> [String]
+score session =
+    map report $ Map.toList $ G.gamescore session
+  where
+    report (player, score) = concat
+        [ name player, ": "
+        ,        show      (G.territorypoint score), " (territory)"
+        , " + ", show      (G.capturepoint score),   " (captures)"
+        , " + ", showFloat (G.komipoint score),      " (komi)"
+        , " = ", showFloat (G.total score)
+        ]
+    showFloat t = show (fromRational t :: Float)
+
 -- |
-renderer :: Images -> G.GoGameSession Player -> Picture
-renderer images session@G.GoGameSession {G.board = b} =  pictures
-    [ imgwood images
-    , drawgrid 32 (19 - 1) (19 - 1)
-    , drawpieces 32 images b]
+renderer :: Images -> GameState Player -> Picture
+renderer images state@GameState {game = session}
+    | isGameOver state = pictures [ gameover, player1, player2]
+    | otherwise = pictures  [ imgwood images
+                            , drawgrid 32 (19 - 1) (19 - 1)
+                            , drawpieces 32 images $ G.board session]
+    where report = score session
+          gameover = Translate (-170) (-20) $ Scale 0.5 0.5 $ Text "GameOver"
+          player1 = Translate (-200) (-100) $ Scale 0.1 0.1 $ Text $ head report
+          player2 = Translate (-200) (-150) $ Scale 0.1 0.1 $ Text $ report !! 1
 
 type Size = Float
 
@@ -96,12 +120,17 @@ drawpieces cellsize images board = applyViewPortToPicture viewPort $ pictures
 
 updater _ = id
 
-handler (EventKey (MouseButton LeftButton) Down _ mouse) session =
-    case G.move cell session of Right s -> s
-                                Left _  -> session
+handler (EventKey (MouseButton LeftButton) Down _ mouse) state@GameState{game = session}
+    | isGameOver state = initialState
+    | otherwise = case G.move cell session of
+        Right s         -> GameState {game = s, isGameOver = False}
+        Left G.GameOver -> GameState {game = session, isGameOver = True}
+        Left _          -> state
     where cell = screenToGocell mouse
-handler (EventKey (MouseButton RightButton) Down _ mouse) session =
-    case G.pass session of Right s -> s
-                           Left _  -> session
 
-handler _ session = session
+handler (EventKey (MouseButton RightButton) Down _ mouse) state@GameState{game = session} =
+    case G.pass session of Right s         -> GameState {game = s, isGameOver = False}
+                           Left G.GameOver -> GameState {game = session, isGameOver = True}
+                           Left _          -> state
+
+handler _ state = state
